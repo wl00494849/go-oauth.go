e96005d82fea2504a1421317e5e55489a31bd7dd
AKA迪迪弄 <wl00494849@gmail.com>
2022-04-09 21:29:56 +0800

    fix

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	oauthserver "go-oauth.go/oauth-server"
+)
+
 func main() {
+	app := gin.Default()
+
+	oauth := app.Group("oauth")
+	{
+		oauth.GET("/google", oauthserver.GoogleOauthLogin)
+	}
+
+	callback := app.Group("callback")
+	{
+		callback.GET("/google", oauthserver.GoogleCallBack)
+	}
 }
